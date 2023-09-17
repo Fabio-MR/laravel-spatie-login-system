17c10f5bdaed686c5d15415bdd1eb405fecacdd1
Fabio Mendes Ribeiro <40776202+Fabio-MR@users.noreply.github.com>
2023-09-17 18:24:48 -0300

    Adicionei uma mensagem na tela inicial para usuarios que nao possuem nem uma função definida

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
     //
     public function index()
     {
+        $user = auth()->user();
+
+        // Obtém todas as permissões associadas ao usuário
+        $permissions = $user->permissions;
+        $roleNames = $user->getRoleNames();
+
+        if ($permissions->isEmpty() && $roleNames->isEmpty()) {
+            // O usuário não tem permissões, exiba a mensagem personalizada
+            return view('home.index')->with('message', 'Entre em contato com o administrador do sistema para obter permissões.');
+        }
+        
+        // $permissions = Permission::all();
+		// $roles = Role::all();
         return view('home.index');
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,4 +4,11 @@
 class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 <h1 class="h2">Pagina incial</h1>
 </div>
+
+@if(isset($message))
+    <div class="alert alert-warning">
+        {{ $message }}
+    </div>
+@endif
+
 @endsection
